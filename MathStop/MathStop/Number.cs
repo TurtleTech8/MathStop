1fdcf8d347c8bfc8b4a192a53269182b90244856
TurtleTech8 <58091353+TurtleTech8@users.noreply.github.com>
2019-11-22 23:08:48 -0600

    Add files via upload

---
+++ MathStop/MathStop/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathStop
+{
+   class Number
+   {
+      public string Snumb = string.Empty;
+      private int numb = 0;
+   }
+}
